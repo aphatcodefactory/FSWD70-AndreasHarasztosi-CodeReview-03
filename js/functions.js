5f6d1adc5571720f59587479474d059ed1014b2b
Andreas Harasztosi <aph@gmx.at>
2019-05-25 18:38:52 +0200

    final commit; can not find the error NaN

--- js/functions.js
+++ js/functions.js
@@ -1,29 +1,30 @@
 // get input values
-function getInputValues(id1, id2, id3, id4) {
-  var id1, id2, id3, id4;
-  var name = document.getElementById(id1).value;
-  var country = document.getElementById(id2).value;
-  var horsePower = document.getElementById(id3).value;
-  var age = document.getElementById(id4).value;
-  return inputValues = [name, country, number(horsePower), number(age)];
+var id1, id2, id3, id4;
+
+function getInputValues() {
+  var name = document.getElementById('name').value;
+  var country = document.getElementById('country').value;
+  var horsePower = document.getElementById('horsePower').value;
+  var age = document.getElementById('age').value;
+  return inputValues = [name, country, parseInt(horsePower), parseInt(age)];
 }
 
 // calculate insurance
 function calculateInsurance(country, horsePower, age) {
   switch (country) {
     case 'Hungary':
-      return horsePower * 120 / age  + 100;
+      return parseInt(horsePower * 120 / age  + 100);
       break;
     case 'Greece':
-      return horsePower * 150 / (age + 3)  + 50;
+      return parseInt(horsePower * 150 / (age + 3)  + 50);
       break;
     default:
-      return horsePower * 100 / age  + 50;
+      return parseInt(horsePower * 100 / age  + 50);
   }
 }
 
 // print insurance info
 function resultOfInsurance() {
   document.getElementById('insuranceResult').innerHTML =
-      inputValues[0] + ', your insurance costs &euro; ' + calculateInsurance() + '.';
+      inputValues[0] + ', your insurance costs &euro; ' + parseInt(calculateInsurance()) + '.';
 }
