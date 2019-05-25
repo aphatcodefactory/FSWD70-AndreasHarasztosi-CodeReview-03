bed53bcd28dc2b294aa3dce2a8e2916ba9831277
Andreas Harasztosi <aph@gmx.at>
2019-05-25 15:43:57 +0200

    refactorised in functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,10 @@
 // get input values
-function getInputValues() {
-  var name = document.getElementById('name').value;
-  var country = document.getElementById('country').value;
-  var horsePower = document.getElementById('horsePower').value;
-  var age = document.getElementById('age').value;
+function getInputValues(id1, id2, id3, id4) {
+  var id1, id2, id3, id4;
+  var name = document.getElementById(id1).value;
+  var country = document.getElementById(id2).value;
+  var horsePower = document.getElementById(id3).value;
+  var age = document.getElementById(id4).value;
   return inputValues = [name, country, horsePower, age];
 }
 
